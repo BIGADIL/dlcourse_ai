8b8c039fb14b63c189195e0003c6aad341c85e80
BIGADIL <morshinin.alexander@gmail.com>
2020-01-19 22:36:34 +0600

    Add solution for KNN-assignment

--- assignments/assignment1/metrics.py
+++ assignments/assignment1/metrics.py
@@ -9,16 +9,29 @@ def binary_classification_metrics(prediction, ground_truth):
     Returns:
     precision, recall, f1, accuracy - classification metrics
     '''
+    
+    assert len(prediction) == len(ground_truth)
+    
     precision = 0
     recall = 0
     accuracy = 0
     f1 = 0
-
-    # TODO: implement metrics!
-    # Some helpful links:
-    # https://en.wikipedia.org/wiki/Precision_and_recall
-    # https://en.wikipedia.org/wiki/F1_score
     
+    total = len(prediction)
+    tp = 0
+    fp = 0
+    fn = 0
+    for i in range(total):
+        y = ground_truth[i]
+        y_pred = prediction[i]
+        tp += 1 if y and y_pred else 0
+        fp += 1 if not y and y_pred else 0
+        fn += 1 if y and not y_pred else 0
+        accuracy += 1 if y == y_pred else 0
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    f1 = 2 * precision * recall / (precision + recall)
+    accuracy = accuracy / total
     return precision, recall, f1, accuracy
 
 
@@ -33,5 +46,9 @@ def multiclass_accuracy(prediction, ground_truth):
     Returns:
     accuracy - ratio of accurate predictions to total samples
     '''
-    # TODO: Implement computing accuracy
-    return 0
+    assert len(prediction) == len(ground_truth)
+    accuracy = 0
+    total = len(prediction)
+    for i in range(total):
+        accuracy += 1 if prediction[i] == ground_truth[i] else 0
+    return accuracy / total
